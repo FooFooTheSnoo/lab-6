d3e0591b38d6beaf241bb9305212fc9b4ec88f47
Joshua Hahn <114379383+FooFooTheSnoo@users.noreply.github.com>
2023-02-23 19:12:16 -0800

    js functions and DOM elements complete

--- js/app.js
+++ js/app.js
@@ -1,1 +1,91 @@
 'use strict';
+
+let sectionElem = document.getElementById('cookie-stores');
+
+const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+
+
+
+let seattle = {
+  city: 'Seattle',
+  min: 23,
+  max: 65,
+  avgAmountSale: 6.3,
+  cookiesPerHour: [],
+  storeTotal: 0,
+  getCustomer: function () {
+    return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
+  },
+  cookiesSold: function () {
+    for (let i = 0; i < hours.length; i++) {
+      let customer = this.getCustomer();
+      let hourlyTotal = Math.floor(customer * this.avgAmountSale);
+      this.cookiesPerHour.push(hourlyTotal);
+      this.storeTotal += hourlyTotal;
+    }
+  },
+  render: function () {
+    let articleElem = document.createElement('article');
+    sectionElem.appendChild(articleElem);
+
+    // h3 = name of city
+    let h3Elem = document.createElement('h3');
+    h3Elem.textContent = this.seattle;
+    articleElem.appendChild(h3Elem);
+
+    let pElem = document.createElement('p');
+    pElem.textContent = this.city;
+    articleElem.appendChild(pElem);
+
+    let ulElem = document.createElement('ul');
+    articleElem.appendChild(ulElem);
+    for (let i = 0; i < this.cookiesPerHour.length; i++) {
+      let liElem = document.createElement('li');
+      liElem.textContent = `${hours[i]}: ${this.cookiesPerHour[i]} cookies`;
+      ulElem.appendChild(liElem);
+    }
+    
+    let pElem2 = document.createElement('p');
+    pElem2.textContent = `Total: ${this.storeTotal} cookies sold`;
+    articleElem.appendChild(pElem2);
+  }
+};
+seattle.cookiesSold();
+console.log(seattle.cookiesPerHour, seattle.storeTotal);
+seattle.render();
+
+
+
+// call the cookie function inside of the render, then call the render function
+
+
+// let tokyo = {
+//   city: 'tokyo',
+//   customerMin: '3',
+//   customerMax: '24',
+//   AvgCookieSale: '1.2',
+//   SeattleAvgArr: [...] //display value os 'ul' in browswer
+// }
+
+// let dubai = {
+//   city: 'dubai',
+//   customerMin: '11',
+//   customerMax: '38',
+//   AvgCookieSale: '3.7',
+//   SeattleAvgArr: [...] //display value os 'ul' in browswer
+// }
+
+// let paris = {
+//   city: 'Seattle',
+//   customerMin: '20',
+//   customerMax: '38',
+//   AvgCookieSale: '2.3',
+//   SeattleAvgArr: [...] //display value os 'ul' in browswer
+// }
+
+// let lima = {
+//   city: 'Seattle',
+//   customerMin: '2',
+//   customerMax: '16',
+//   AvgCookieSale: '4.6',
+//   SeattleAvgArr: [...] //display value os 'ul' in browswer
